9c12a3767d25d6b3a2d0994fc528fe1c41feb770
Ugo Aniukwu <ugo@smallhaxe.com>
2015-05-12 09:46:58 -0700

    mockdata rendered, unpretty

--- base.js
+++ base.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
   // code in here!
   console.log("here's your data:", mockData)
+	var data = mockData.data;
+	var $fHurls = [];
+
+	function loadGallery(data) {
+	    data.forEach(function(val, ind, arr) {
+	        $fHurls.push(val.images.fixed_height.url)
+	    }); return $fHurls;
+	//     $fHurls.each(function(ind, ele) {
+	//         $('body').append('<img src="' + ele + '">');
+	//     });
+	}
+
+	function render(imgArr) {
+	    for (var i = 0; i < imgArr.length; i++) {
+	        $('body').append('<img src="' + imgArr[i] + '">');
+	    }
+	}
+
+	loadGallery(data);
+	render($fHurls)
 })
